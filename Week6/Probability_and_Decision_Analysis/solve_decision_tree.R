857d6855bde84aa2c0e4c76a12756ba616c3203c
Yasmeen Mubarak <37984450+Yasmeenmad@users.noreply.github.com>
2021-05-04 14:39:24 +0300

    Add files via upload

--- Week6/Probability_and_Decision_Analysis/solve_decision_tree.R
+++ Week6/Probability_and_Decision_Analysis/solve_decision_tree.R
@@ -1,7 +1,10 @@
-    library('tidyverse')
-    library('plyr'); library('dplyr')
-    library('reshape')
+    # ===========================================================================================================================================
+    # ------------------------------------------- Ideal Solution For The Kim Birthday Party Decision Tree --------------------------------------
+    # ===========================================================================================================================================
     
+    # library we need
+    library('tidyverse')
+
     # values for each column in kim birthday party
     location_output    <- c("Outdoors", "Porch", "Indoors")
     weather_output     <- c("Sunny", "Rainy")
@@ -14,10 +17,11 @@
     kim_party <- data.frame(locations, weathers)
     
     
-    # create 
+    # adding a new column depending on the values of weathers column
     kim_party <- kim_party %>% mutate(weather_probability = case_when(weathers == "Sunny" ~ 0.4,
                                                                               weathers == "Rainy" ~ 0.6 ))
     
+    # adding a new column depending on the values of locations and weathers columns 
     kim_party <- kim_party %>% mutate(weight = case_when((locations == "Outdoors" & weathers == "Sunny") ~ 100,
                                                       (locations == "Outdoors" & weathers == "Rainy") ~ 0,
                                                       (locations == "Porch" & weathers == "Sunny") ~ 90,
@@ -25,29 +29,44 @@
                                                       (locations == "Indoors" & weathers == "Sunny") ~ 40,
                                                       (locations == "Indoors" & weathers == "Rainy") ~ 50))
 
-    # create a 
+    
+    #create a function to print out the ideal solution which's the maximum of value_path
     ideal_solution <- function(){
       
+      # Initial list
+      mylist <- list()
+      
+      #for-loop to printing the all values for each party location
+      # find the value for each path by calculating the sum of the probability in each location
       for(location in location_output){
         
-      same_location <- kim_party[kim_party$locations == location,]
-      value_path           <- list(sum(same_location$weather_probability * same_location$weight))
-     #mylist <- list(sum(kim_party[kim_party$location == i,]$weather_probability * kim_party[kim_party$location == i,]$weight))
-     #new_mat <- rbind(mylist[i])
-     
-    print (same_location)
-    print(value_path)
-    
-    #lapply(sum[i],FUN=max)
-    #print(new_mat)
+        # save the output for the each location to same_location to print it
+        same_location <- kim_party[kim_party$locations == location,]
+        
+        # determine how much each party location is worth based on the values provided by Kim
+        # save the sum of the decision tree probability for the same location
+        value_path           <- list(sum(same_location$weather_probability * same_location$weight))
+        
+        # calculate how much each uncertainty decision is worth then append it to a list
+        mylist[[length(mylist)+1]] <- list(sum(same_location$weather_probability * same_location$weight))
+         
+        # show rows that have the same location 
+         print (same_location)
+         
+         # print out the value of much each uncertainty decision is worth
+         print(value_path)
+      
       }
-      #mylist <- list(sum)
-      #max(sapply(new_mat, max))
-      max(unlist(value_path))
-      #max(unlist(lapply(sum[i],FUN=max)))
-      #lapply(sum[i],FUN=function(x)apply(x,MARGIN=1,FUN=max))
-      #lapply(sum[i],FUN=max)
-      #max(sum)
+      # appand the list
+      mylist
+      
+      print("The ideal solution for kim is to choose the location for her party whose value will be:")
+      
+      # choose the ideal solution for the kim party decision tree
+      # which is the maximum value based on the calculated results of the decision tree
+      max(unlist(mylist))
     }
+    
+    # show the final result
     ideal_solution()
     
